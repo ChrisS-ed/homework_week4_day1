bf5799d4ab61ccf75b603c5316c96b3ef45f919f
ChrisS-ed <c.sloane@hotmail.co.uk>
2015-12-07 20:07:05 +0000

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,18 @@
+class HomeController < ApplicationController
+
+  def welcome
+  end
+
+  def sports_cars
+  end
+
+  def budget_cars
+  end
+
+  def specialist_vehicles
+  end
+
+  def car_accessories
+  end
+
+end
